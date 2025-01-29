de5a13b213afa4c8b50e51005c3498f81a208484
Giocatory <nivolyas@gmail.com>
2025-01-29 14:59:46 +0300

    admin view

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UsersMessages
 
-# Register your models here.
+
+@admin.register(UsersMessages)
+class UsersMessagesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'message', 'email', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'message', 'email')
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
+    readonly_fields = ('id', 'created_at')

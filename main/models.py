db321f22d02956278760c738d306add33ce4737b
Giocatory <nivolyas@gmail.com>
2025-01-29 14:50:19 +0300

    save users comments

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class UsersMessages(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    message = models.TextField()
+    email = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "users_messages"
+        ordering = ['-created_at']
+        verbose_name = "User's Message"
+        verbose_name_plural = "User's Messages"
--- main/views.py
+++ main/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
+from .models import UsersMessages
+
 
 def main(request):
-    user_name = request.GET.get('user_name')
-    user_email = request.GET.get('user_email')
-    user_text = request.GET.get('user_text')
+    if request.method == 'POST':
+        # user_name = request.GET.get('user_name')
+        # user_email = request.GET.get('user_email')
+        # user_text = request.GET.get('user_text')
+
+        message = UsersMessages()
+        message.name = request.POST.get('user_name')
+        message.email = request.POST.get('user_email')
+        message.message = request.POST.get('user_text')
+        message.save()
 
     return render(request, 'index.html')
